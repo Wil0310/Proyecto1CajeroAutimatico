e3744dd3322a1b8e05c8a46a80a0c5f54da1e603
K1ze050 <aguilonandres9@gmail.com>
2025-08-18 21:49:57 -0600

    Eliminación del error de entrada de valor textual en vez de numerico

--- SaldoyDeposito.py
+++ SaldoyDeposito.py
@@ -4,6 +4,9 @@ def consultar_saldo(): #Consulta el saldo existente en la cuenta
     print(f"Saldo actual: {saldo:.2f}")  
 
 def depositar(): 
-    monto = int(input("Monto a depositar: "))
+    try:
+        monto = int(input("Monto a depositar: "))
+    except ValueError:
+        print("Porfavor, coloque un valor numerico, no textual")
     saldo += monto  
     print("¡Depósito exitoso!")
