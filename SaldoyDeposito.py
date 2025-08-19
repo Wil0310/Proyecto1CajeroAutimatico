be646e9e7a14fcbf20e0eceb1049ad950c2d9b4d
K1ze050 <aguilonandres9@gmail.com>
2025-08-18 21:42:07 -0600

    Creación de la opción deposito

--- SaldoyDeposito.py
+++ SaldoyDeposito.py
@@ -1,4 +1,9 @@
 saldo = 1000  #Creación del saldo inicial
 
 def consultar_saldo(): #Consulta el saldo existente en la cuenta
-    print(f"Saldo actual: {saldo}")  
+    print(f"Saldo actual: {saldo:.2f}")  
+
+def depositar(): 
+    monto = int(input("Monto a depositar: "))
+    saldo += monto  
+    print("¡Depósito exitoso!")
