5874731c944e4e2b68f0ccdd1e1ad221bf9c3740
K1ze050 <aguilonandres9@gmail.com>
2025-08-18 22:33:28 -0600

    Agregación del SaldoyDeposito

---
+++ SaldoyDeposito.py
@@ -0,0 +1,12 @@
+saldo = 1000  #Creación del saldo inicial
+
+def consultar_saldo(): #Consulta el saldo existente en la cuenta
+    print(f"Saldo actual: {saldo:.2f}")  
+
+def depositar(): #Creación de la opción de deposito que nos permitira depositar en la cuenta
+    try: #Try nos ayudara a hacer una corrección en cado caso el monto ingresado no sea tipo flotante
+        monto = float(input("Monto a depositar: "))
+    except ValueError:
+        print("Porfavor, coloque un valor numerico, no textual")
+    saldo += monto  
+    print("¡Depósito exitoso!") #Impresión de mensaje de finalización y basicamente cierre de la funcion depositar
